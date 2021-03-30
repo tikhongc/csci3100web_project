1c5130b89a10d5b54fa6a546551ed3ab9d72a90a
tikhongc <77725910+tikhongc@users.noreply.github.com>
2021-03-30 20:17:48 +0800

    Update mongoose.js

--- mongodb/mongoose.js
+++ mongodb/mongoose.js
@@ -1,3 +1,14 @@
 mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/QUERY-user-api',
-{useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true});
+const url ="mongodb+srv://tedchiu:tedchiu@cluster0.ye8ch.mongodb.net/cuery?retryWrites=true&w=majority";
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
