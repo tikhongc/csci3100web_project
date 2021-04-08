5b14a50f0065da32c9fdd71ad443313fb5600420
tikhongc <77725910+tikhongc@users.noreply.github.com>
2021-04-08 23:27:51 +0800

    Update CommentController.js

--- CUERY/Post_System/CommentController.js
+++ CUERY/Post_System/CommentController.js
@@ -55,8 +55,11 @@ router.get('/comments/children/:id', authentication, async (req, res) => {
 
 //3. Create a comment
 router.post('/comments', authentication, async (req, res) => {
-    const newComment = new CommentModel(req.body);
 
+    const newComment = new CommentModel({
+        ...req.body,
+        owner: req.user._id,
+    });
 
     try {
         const parentPost = await PostModel.findById(newComment.parentPost);
