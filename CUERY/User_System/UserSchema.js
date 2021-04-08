e9b3e9726d9b0005ccd9665c9ebf270ec23f3e79
tikhongc <77725910+tikhongc@users.noreply.github.com>
2021-04-08 23:13:20 +0800

    Update UserSchema.js

--- CUERY/User_System/UserSchema.js
+++ CUERY/User_System/UserSchema.js
@@ -2,21 +2,11 @@
 //code for user input when they sign in
 
 
-const mongoose = require('mongoose');//https://mongoosejs.com/docs/index.html
-require('../mongodb/mongoose');//mongoose: https://developer.mozilla.org/zh-TW/docs/Learn/Server-side/Express_Nodejs/mongoose
-const validator = require('validator');//https://www.npmjs.com/package/validator
-const bcrypt = require('bcryptjs');//https://www.npmjs.com/package/bcrypt
+const mongoose = require('mongoose');
+require('../mongodb/mongoose');
+const validator = require('validator');
 
-//var passwordValidator = require('password-validator');
-//var schema = new passwordValidator();//https://tarunbatra.com/password-validator/5.1.1/
-
-//mongoose它定義了一個schema來表示document的數據結構或者構造函數,Schema允許您定義存儲在每個文檔中的字段，及其驗證要求和默認值。
-//Models是從Schema編譯來的構造函數。它們的實例就代表著可以從數據庫保存和讀取的documents。從數據庫創建和讀取document的所有操作都是通過model進行的
-
-//user need to input:1.name 2.password 3.email 6.acdemic year 
-//haven't finished: 4.identity: select student or professor 5.select major 
-
-const UserSchema= new mongoose.Schema({//mongoose schematypes:https://mongoosejs.com/docs/schematypes.html
+const UserSchema= new mongoose.Schema({
 
     //user name  : 1.no-break space 2.string type 3.min 3 words 4. must input 5.unique
     name: {
@@ -31,8 +21,7 @@ const UserSchema= new mongoose.Schema({//mongoose schematypes:https://mongoosejs
     password: {
         type: String,
         required: true,
-        //minlength: 8,
-       //maxlength:18,
+        minlength: 8,
         trim: true,
     },
 
@@ -86,8 +75,19 @@ const UserSchema= new mongoose.Schema({//mongoose schematypes:https://mongoosejs
         type: Date,
         required: false
     },
-     //major:{}
-    //identity:{}
+    faculty:{
+        type: String,
+        required: false,
+        enum: ["Faculty of Arts","Faculty of Business Administration","Faculty of Education","Faculty of Law","Faculty of Medicine","Faculty of Science","Faculty of Social Science","Faculty of Social Science","Graduate School","Other Academic Units"],
+    },
+     identity:{
+        type: String,
+        required: false,
+        enum: ["student","professor"]
+     },
+    // perference:{
+       
+    // },
 }, {timestamps: true});
 
 module.exports = UserSchema;
