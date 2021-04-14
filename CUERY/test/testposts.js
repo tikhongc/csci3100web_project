186c4805af137d2218ac46c07785c434c85ba95d
WaiKitChan <50606425+WaiKitChan@users.noreply.github.com>
2021-04-14 23:47:46 +0800

    Update testposts.js

--- CUERY/test/testposts.js
+++ CUERY/test/testposts.js
@@ -1,8 +1,8 @@
 // run node testposts.js to generate test posts
 
 require('./mongodb/mongoose');
-const PostModel = require("./Post_Systen/PostModel.js");
-const {categoryList,topicList} = require("./Post_Systen/PostSchema.js");
+const PostModel = require("./Post_System/PostModel.js");
+const {categoryList,topicList} = require("./Post_System/PostSchema.js");
 
 const cl = categoryList.length, tl = topicList.length;
 
