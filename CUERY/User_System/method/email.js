a59de1f0d17347da9c44e3b4fdb99964467dfbf2
tikhongc <1205542199@qq.com>
2021-04-15 00:19:19 +0800

    n

--- CUERY/User_System/method/email.js
+++ CUERY/User_System/method/email.js
@@ -1,8 +1,9 @@
 //file for sending email to user
 //using send grid to send email //https://app.sendgrid.com/guide
+const sgMail = require('@sendgrid/mail')
 
-const Mail = require('@sendgrid/mail');
-Mail.setApiKey(process.env.APIkey);
+//sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
 
 // types of email 
 
@@ -11,7 +12,7 @@ Mail.setApiKey(process.env.APIkey);
 //notification email ?
 
 const WelcomeEmail = (email, name) => {
-    Mail.send({
+  sgMail.send({
         to: email,
         from: 'chiutikhong@yandex.com',
         subject: 'Thanks for joining in Cuery community!',
@@ -26,7 +27,7 @@ const WelcomeEmail = (email, name) => {
 }
 
 const RecoveryEmail = (email,name,link) => {
-    Mail.send({
+  sgMail.send({
         to: email,
         from: 'chiutikhong@yandex.com',
         subject: 'Account Activation Link',
@@ -42,7 +43,7 @@ const RecoveryEmail = (email,name,link) => {
 }
 
 const ConfirmationEmail = (email,name) => {
-  Mail.send({
+  sgMail.send({
       to: email,
       from: 'chiutikhong@yandex.com',
       subject: "Your password has been changed",
